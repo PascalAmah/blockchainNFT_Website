2e409161fea348c5551d3a3c1b4dee13aa9fcb0f
PascalAmah <pascal.amah10@gmail.com>
2024-08-13 18:14:23 +0100

    Privacy page completed, homepage updated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
   "use strict";
 
   document.addEventListener("DOMContentLoaded", function () {
-
     // Load header and footer, then load the initial page
     Promise.all([
       fetch("partials/header.html").then((response) => response.text()),
@@ -18,6 +17,7 @@
         // Add event listeners to navigation links
         addNavigationListeners();
         attachEventListeners();
+        // toggleMenu();
       })
       .catch((error) => {
         console.error("Error loading header or footer:", error);
@@ -37,21 +37,27 @@
           setTimeout(() => {
             document.getElementById("main-content").innerHTML = data;
             loader.style.display = "none";
+
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth", // Adds a smooth scroll effect
+            });
+
             document.getElementById("main-content").classList.remove("hidden");
             addNavigationListeners();
             attachEventListeners();
+            toggleMenu();
 
             loader.addEventListener(function () {
               loader.style.display = "none"; // Loader end
             });
           }, 2000);
         })
-        
+
         .catch((error) => {
           console.error("Error loading page:", error);
           loader.style.display = "none"; // Hide loader on error
         });
-        
     }
 
     // Add event listeners to navigation links
@@ -66,7 +72,9 @@
       });
 
       // Add event listeners to links within the dynamically loaded pages
-      const navLinks = document.querySelectorAll("#header .profile_img a, #header .logo a");
+      const navLinks = document.querySelectorAll(
+        "#header .profile_img a, #header .user_profile a, .logo a, .footer_link a, .cta_btn a"
+      );
       navLinks.forEach((link) => {
         link.addEventListener("click", function (event) {
           event.preventDefault(); // Prevent default link behavior
@@ -84,7 +92,6 @@
       });
     }
 
-
     // Profile Update function
     function attachEventListeners() {
       const profileUpdateButton = document.getElementById("profileUpdate");
@@ -93,7 +100,6 @@
 
       if (profileUpdateButton) {
         profileUpdateButton.addEventListener("click", function () {
-
           // Show the modal
           modal.classList.add("show");
 
@@ -116,6 +122,66 @@
         if (event.target === modal) {
           modal.classList.remove("show");
         }
+      });
+    }
+
+    // Navbar toggle open
+    function toggleMenu() {
+      const checkbox = document.getElementById("check");
+      const menu = document.getElementById("menu");
+      const content = document.getElementById("main-content");
+      const menuItems = document.querySelectorAll("#menu a");
+
+      // Event listener to toggle the menu open/close
+      checkbox.addEventListener("change", function () {
+        if (this.checked) {
+          menu.classList.add("open");
+          content.classList.add("blur");
+        } else {
+          menu.classList.remove("open");
+          content.classList.remove("blur");
+        }
+      });
+
+      // Close the menu if clicked outside
+      window.addEventListener("click", function (event) {
+        if (
+          !menu.contains(event.target) &&
+          event.target !== checkbox &&
+          event.target.closest("label") === null
+        ) {
+          checkbox.checked = false;
+          menu.classList.remove("open");
+          content.classList.remove("blur");
+        }
+      });
+
+      // Close the menu when a menu item is clicked
+      menuItems.forEach((item) => {
+        item.addEventListener("click", (event) => {
+          checkbox.checked = false;
+          menu.classList.remove("open");
+          content.classList.remove("blur");
+
+          // Smooth scroll to the section
+          const targetId = item.getAttribute("href");
+          if (targetId) {
+            event.preventDefault();
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+              const headerOffset =
+                document.querySelector("header").offsetHeight;
+              const elementPosition =
+                targetElement.getBoundingClientRect().top + window.pageYOffset;
+              const offsetPosition = elementPosition - headerOffset;
+
+              window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth",
+              });
+            }
+          }
+        });
       });
     }
   });
